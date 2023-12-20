914ffbdc9fe8cc66a91a2a9dadfdbab9bd4eaef6
José Divino <josedivinooficial02@gmail.com>
2023-12-19 21:17:34 -0300

    crm-1: send welcome notification

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\Auth\Register;
 use App\Models\User;
+use App\Notifications\WelcomeNotification;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Notification;
 use Livewire\Livewire;
 
 use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
@@ -64,3 +66,18 @@ test('validation rules', function ($f) {
     'email::unique'      => (object) ['field' => 'email', 'value' => 'josedivino@divino.com', 'rule' => 'unique', 'aField' => 'email_confirmation', 'aValue' => 'josedivino@divino.com'],
     'password::required' => (object) ['field' => 'password', 'value' => '', 'rule' => 'required'],
 ]);
+
+it("should send a notification welcoming the new user", function () {
+    Notification::fake();
+
+    Livewire::test(Register::class)
+        ->set('name', 'Jose Divino')
+        ->set('email', 'josedivino@divino.com')
+        ->set('email_confirmation', 'josedivino@divino.com')
+        ->set('password', 'password')
+        ->call('submit');
+
+    $user = User::whereEmail('josedivino@divino.com')->first();
+
+    Notification::assertSentTo($user, WelcomeNotification::class);
+});
