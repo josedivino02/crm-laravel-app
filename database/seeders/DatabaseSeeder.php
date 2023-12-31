36f1e5caf0df301d3b1aba3d0ad2ee4f555dcdd6
José Divino <josedivinooficial02@gmail.com>
2023-12-31 15:41:27 -0300

    crm-5: creating a seed for permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,19 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            PermissionSeeder::class,
+        ]);
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name'  => 'Divino',
             'email' => 'jose@divino.com',
         ]);
