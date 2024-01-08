c91b483d3899e7bda019d6fe0f9d553ef9e9b71a
José Divino <josedivinooficial02@gmail.com>
2024-01-08 20:23:22 -0300

    crm-15: be able to restore a user

---
+++ resources/views/livewire/admin/users/restore.blade.php
@@ -0,0 +1,15 @@
+<x-modal wire:model="modal" title="Restore confirmation"
+    subtitle="You are restoring access for the user {{ $user?->name }}" separator>
+    @error('confirmation')
+        <x-alert icon="o-exclamation-triangle" class="alert-error mb-4">
+            {{ $message }}
+        </x-alert>
+    @enderror
+
+    <x-input class="input-sm" label=" Write `CONFIRMAR R` to confirm the restoration"
+        wire:model="confirmation_confirmation" />
+    <x-slot:actions>
+        <x-button label="Cancel" @click="$wire.modal = false" />
+        <x-button label="Confirm" class="btn-primary" wire:click='restore' />
+    </x-slot:actions>
+</x-modal>
