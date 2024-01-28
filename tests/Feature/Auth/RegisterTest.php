aa4a6870ba8acade5aaf741d8155ab66ae9f1bb2
José Divino <josedivinooficial02@gmail.com>
2024-01-28 14:21:21 -0300

    crm-11: making sure that registration process is dispatching registered event

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -4,7 +4,8 @@ use App\Livewire\Auth\Register;
 use App\Models\User;
 use App\Notifications\WelcomeNotification;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Support\Facades\Notification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\{Event, Notification};
 use Livewire\Livewire;
 
 use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
@@ -80,4 +81,17 @@ it("should send a notification welcoming the new user", function () {
     $user = User::whereEmail('josedivino@divino.com')->first();
 
     Notification::assertSentTo($user, WelcomeNotification::class);
+});
+
+it("should dispatch Registered event", function () {
+    Event::fake();
+
+    Livewire::test(Register::class)
+        ->set('name', 'Jose Divino')
+        ->set('email', 'josedivino@divino.com')
+        ->set('email_confirmation', 'josedivino@divino.com')
+        ->set('password', 'password')
+        ->call('submit');
+
+    Event::assertDispatched(Registered::class);
 });
