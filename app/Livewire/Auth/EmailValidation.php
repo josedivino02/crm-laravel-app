139eef8aaace4406f29af8a2b9b767341f655448
José Divino <josedivinooficial02@gmail.com>
2024-02-04 20:55:29 -0300

    crm-11: front-end: creating our email-validation page

--- app/Livewire/Auth/EmailValidation.php
+++ app/Livewire/Auth/EmailValidation.php
@@ -7,12 +7,16 @@ use App\Notifications\WelcomeNotification;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class EmailValidation extends Component
 {
     public ?string $code = null;
 
+    public ?string $sendNewCodeMessage = null;
+
+    #[Layout('components.layouts.guest')]
     public function render(): View
     {
         return view('livewire.auth.email-validation');
@@ -20,6 +24,7 @@ class EmailValidation extends Component
 
     public function handle(): void
     {
+        $this->reset('sendNewCodeMessage');
         $this->validate([
             'code' => function (string $attribute, mixed $value, Closure $fail) {
                 if ($value != auth()->user()->validation_code) {
@@ -40,5 +45,7 @@ class EmailValidation extends Component
     public function sendNewCode(): void
     {
         SendNewCode::dispatch(auth()->user());
+
+        $this->sendNewCodeMessage = 'Code was sent to you. Check your mailbox.';
     }
 }
--- resources/views/livewire/auth/email-validation.blade.php
+++ resources/views/livewire/auth/email-validation.blade.php
@@ -1,3 +1,24 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+<x-card title="Email Validation" class="mx-auto w-[450px]">
+
+    @if ($sendNewCodeMessage)
+        <x-alert icon="o-envelope" class="alert-warning mb-4">
+            {{ $sendNewCodeMessage }}
+        </x-alert>
+    @endif
+
+
+    <x-form wire:submit="handle">
+        <p>We sent you a code. Please check your email.</p>
+        <x-input label="Code" wire:model="code" />
+        <x-slot:actions>
+            <div class="w-full flex items-center justify-between">
+                <a wire:click="sendNewCode" class="link-primary">
+                    Send a new code
+                </a>
+                <div>
+                    <x-button label="Check Codee" class="btn-primary " type="submit" spinner="submit" />
+                </div>
+            </div>
+        </x-slot:actions>
+    </x-form>
+</x-card>
