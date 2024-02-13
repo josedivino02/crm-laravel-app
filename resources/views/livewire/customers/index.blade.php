ffefdafd42332acf8b55d9238b7860bcfb84859d
José Divino <josedivinooficial02@gmail.com>
2024-02-13 17:19:54 -0300

    crm-28: removing the button when the customer is already archived

--- resources/views/livewire/customers/index.blade.php
+++ resources/views/livewire/customers/index.blade.php
@@ -38,8 +38,10 @@
 
         @scope('actions', $customer)
             <div class="flex items-center space-x-2">
-                <x-button id="archive-btn-{{ $customer->id }}" wire:key='archive-btn-{{ $customer->id }}' icon="o-trash"
-                    @click="$dispatch('customer::archive', {id: {{ $customer->id }} })" spinner class="btn-sm" />
+                @unless ($customer->trashed())
+                    <x-button id="archive-btn-{{ $customer->id }}" wire:key='archive-btn-{{ $customer->id }}' icon="o-trash"
+                        @click="$dispatch('customer::archive', {id: {{ $customer->id }} })" spinner class="btn-sm" />
+                @endunless
             </div>
         @endscope
     </x-table>
