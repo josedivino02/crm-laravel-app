b06607b146ce7d2c78717e514732ac0e60cb56f3
Divino <josedivinooficial02@gmail.com>
2024-04-18 05:48:27 -0300

    CRM-35: sorting by status

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -19,6 +19,14 @@ class Board extends Component
     public function opportunities(): Collection
     {
         return Opportunity::query()
+            ->orderByRaw(
+                "
+                case
+                    when status = 'open' then 1
+                    when status = 'won' then 2
+                    when status = 'lost' then 3
+                end"
+            )
             ->get();
     }
 }
