c39211b12fc8161cd8fa05c8dc75104f07c6fc04
Divino <josedivinooficial02@gmail.com>
2024-04-18 06:53:12 -0300

    CRM-35: board backend

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Opportunities;
 use App\Models\Opportunity;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -19,19 +20,47 @@ class Board extends Component
     public function opportunities(): Collection
     {
         return Opportunity::query()
-            ->orderByRaw(
-                "
-                case
-                    when status = 'open' then 1
-                    when status = 'won' then 2
-                    when status = 'lost' then 3
-                end"
-            )
+            ->orderByRaw("field(status, 'open', 'won', 'lost')")
+            ->orderBy('sort_order')
             ->get();
     }
 
     public function updateOpportunities($data)
     {
 
+        $order = collect();
+
+        foreach ($data as $group) {
+            $order->push(
+                collect($group["items"])
+                    ->map(fn ($i) => $i["value"])
+                    ->join(",")
+            );
+        }
+
+        $open = explode(",", $order[0]);
+        $won  = explode(",", $order[1]);
+        $lost = explode(",", $order[2]);
+
+        $sortOrder = $order->join(",");
+
+        DB::table("opportunities")->whereIn('id', $open)->update(["status" => "open"]);
+        DB::table("opportunities")->whereIn('id', $won)->update(["status" => "won"]);
+        DB::table("opportunities")->whereIn('id', $lost)->update(["status" => "lost"]);
+        DB::table("opportunities")->update(["sort_order" => DB::raw("field(id, $sortOrder)")]);
+
+        // foreach ($data as $group) {
+        //     $status = $group['value'];
+        //     $items = $group['items'];
+
+        //     foreach ($items as $item) {
+        //         $order = $item['order'];
+        //         $id = (int) $item['value'];
+
+        //         Opportunity::query()
+        //             ->whereId($id)
+        //             ->update(['sort_order' => $order, 'status' => $status]);
+        //     }
+        // }
     }
 }
